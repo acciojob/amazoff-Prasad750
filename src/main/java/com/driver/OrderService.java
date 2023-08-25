d51e32b348f0b0244ba3fccda84c84c4ca63a3ee
Prasad750 <prasadchaudhari750@gmail.com>
2023-08-26 00:35:20 +0530

    Attempt5

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Service
 public class OrderService {
 
-    @Autowired
-    OrderRepository orderRepository;
+//    @Autowired
+//    OrderRepository orderRepository;
 
-   //OrderRepository orderRepository=new OrderRepository();
+   OrderRepository orderRepository=new OrderRepository();
     public void addOrder(Order order) {
        orderRepository.addOrder(order);
     }
